4db05f76d93f5ceb569d8910f6f3bcbc64a780e6
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-30 01:49:16 +0300

    last

--- src/palindromSayilar.java
+++ src/palindromSayilar.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class palindromSayilar {
     static boolean isPalindrom(int number) {
         int temp = number, reverse = 0, last;
@@ -13,6 +15,14 @@ public class palindromSayilar {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrom(7));
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        if (isPalindrom(n) == true) {
+            System.out.println(n + " is a palindrome number.");
+        } else {
+            System.out.println(n + " is not a palindrome number.");
+        }
     }
 }
