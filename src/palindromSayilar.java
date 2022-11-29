fbcd3e3a11de23401440e7380db32ab2cb682bef
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-29 21:43:58 +0300

    last

--- src/palindromSayilar.java
+++ src/palindromSayilar.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class palindromSayilar {
+public class palindromSayilar {
+    static boolean isPalindrom(int number) {
+        int temp = number, reverse = 0, last;
+        while (temp != 0) {
+            last = temp % 10;
+            reverse = (reverse * 10) + last;
+            temp /= 10;
+        }
+        if (reverse == number)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(7));
+    }
 }
